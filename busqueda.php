8dad2933d63881330a1268e340a5122343eff767
VictorFer25 <victorfer.perez25@gmail.com>
2019-05-12 19:54:14 -0500

    Version 1

---
+++ busqueda.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+   
+
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="images/favicon.png">
+      <title>ECO HTML</title>
+      <!-- CSS FILES START -->
+      <link href="css/custom.css" rel="stylesheet">
+      <link href="css/color.css" rel="stylesheet">
+      <link href="css/responsive.css" rel="stylesheet">
+      <link href="css/owl.carousel.min.css" rel="stylesheet">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/prettyPhoto.css" rel="stylesheet">
+      <link href="css/all.min.css" rel="stylesheet">
+      <!-- CSS FILES End -->
+   </head>
+   <body>
+      <div class="wrapper home2">
+         <!--Header Start-->
+         <header class="header-style-2">
+            <nav class="navbar navbar-expand-lg">
+               <a class="navbar-brand" href="index-2.html"><img src="images/h2logo.png" alt=""></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="index-2.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Home </a>
+                        <ul class="dropdown-menu" >
+                           <li><a href="index-2.html">Home One</a></li>
+                           <li><a href="home-two.html">Home Two</a></li>
+                           <li><a href="home-three.html">Home Three</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item"> <a class="nav-link" href="about.html">About</a> </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="events-grid.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Events </a>
+                        <ul class="dropdown-menu" >
+                           <li><a href="events-grid.html">Events Grid</a></li>
+                           <li><a href="events-grid-2.html">Events Grid Two</a></li>
+                           <li><a href="events-grid-3.html">Events Grid Three</a></li>
+                           <li><a href="events-list.html">Events List</a></li>
+                           <li><a href="events-list-two.html">Events List Two</a></li>
+                           <li><a href="event-details.html">Event Details</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="causes.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Causes </a>
+                        <ul class="dropdown-menu" >
+                           <li><a href="causes.html">Causes Grid</a></li>
+                           <li><a href="causes-list.html">Causes List</a></li>
+                           <li><a href="causes-details.html">Causes Details</a> </li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="blog.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Blogs </a>
+                        <ul class="dropdown-menu" >
+                           <li><a href="blog.html">Blog Default</a></li>
+                           <li><a href="blog-list.html">Blog List</a> </li>
+                           <li><a href="blog-grid.html">Blog Grid</a></li>
+                           <li><a href="blog-two-col.html">Blog Two Columns</a> </li>
+                           <li><a href="blog-three-col.html">Blog Three Columns</a></li>
+                           <li><a href="blog-details.html">Blog Details</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Pages </a>
+                        <ul class="dropdown-menu" >
+                           <li>
+                              <a href="#">Projects</a>
+                              <ul class="dropdown-menu" >
+                                 <li><a href="projects.html">Projects</a> </li>
+                                 <li><a href="projects-grid.html">Projects Grid</a> </li>
+                                 <li><a href="projects-grid-two.html">Projects Grid Two</a> </li>
+                                 <li><a href="projects-list.html">Projects List</a> </li>
+                                 <li><a href="projects-details.html">Project Details</a> </li>
+                              </ul>
+                           </li>
+                           <li>
+                              <a href="#">Shop</a>
+                              <ul class="dropdown-menu" >
+                                 <li><a href="shop.html">Shop</a> </li>
+                                 <li><a href="shop-two.html">Shop Two</a> </li>
+                                 <li><a href="shop-details.html">Shop Details</a> </li>
+                              </ul>
+                           </li>
+                           <li>
+                              <a href="team.html">Team</a>
+                              <ul class="dropdown-menu" >
+                                 <li><a href="team.html">Team One</a> </li>
+                                 <li><a href="team-two.html">Team Two</a> </li>
+                                 <li><a href="team-details.html">Team Details</a> </li>
+                              </ul>
+                           </li>
+                           <li>
+                              <a href="#">Gallery</a>
+                              <ul class="dropdown-menu" >
+                                 <li><a href="gallery-grid.html">Gallery Grid</a> </li>
+                                 <li><a href="gallery-full.html">Gallery Full</a> </li>
+                                 <li><a href="gallery-masonry.html">Gallery Masonry</a> </li>
+                              </ul>
+                           </li>
+                           <li><a href="testimonials.html">Testimonials</a> </li>
+                           <li><a href="donation.html">Donation</a> </li>
+                           <li><a href="my-account.html">My Account</a> </li>
+                           <li><a href="coming-soon.html">Coming Soon</a> </li>
+                           <li><a href="page-404.html">404 Error</a> </li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="contact.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Contact </a>
+                        <ul class="dropdown-menu" >
+                           <li><a href="contact-one.html">Contact One</a> </li>
+                           <li><a href="contact-two.html">Contact Two</a> </li>
+                        </ul>
+                     </li>
+                  </ul>
+                  <ul class="topnav-right">
+                     <li> <a class="mdonate" href="donation.html"><span>Make a Donation</span></a> </li>
+                     <li> <a class="search-icon" href="#search"> <i class="fas fa-search"></i> </a> </li>
+                     <li class="dropdown">
+                        <a class="cart-icon" href="#" role="button" id="cartdropdown" data-toggle="dropdown"> <i class="fas fa-shopping-cart"></i></a>
+                        <div class="dropdown-menu cart-box" aria-labelledby="cartdropdown">
+                           Recently added item(s)
+                           <ul class="list">
+                              <li class="item">
+                                 <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
+                                 <div class="description"> <a href="#">Sample Course</a> <strong class="price">1 x $44.95</strong> </div>
+                              </li>
+                              <li class="item">
+                                 <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
+                                 <div class="description"> <a href="#">Sample Course</a> <strong class="price">1 x $44.95</strong> </div>
+                              </li>
+                           </ul>
+                           <div class="total">Total: <strong>$44.95</strong></div>
+                           <div class="view-link"><a href="#">Proceed to Checkout</a> <a href="#">View cart </a></div>
+                        </div>
+                     </li>
+                     <li class="login-reg"> <a href="my-account.html">Login</a> | <a href="my-account.html">Signup</a> </li>
+                  </ul>
+               </div>
+            </nav>
+         </header>
+         
+         <section class="wf100 p80 current-projects">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-6">
+                     <div class="section-title-2">
+                        <h2>Recientes</h2>
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <ul class="nav" id="myTab" role="tablist">
+                        <form class="form-inline my-2 my-lg-0" method="POST" action="" onSubmit="return validarForm(this)">
+                           <input class="form-control mr-sm-2" type="text" placeholder="Buscar" name="palabra">
+                           <button class="btn btn-outline-success my-2 my-sm-0" type="submit" value="Buscar" name="buscar">Search</button>
+                         </form>
+                     </ul>
+                  </div>
+               </div>
+               <div class="row">
+                  <?php
+                     $db = new mysqli('192.168.101.49', 'elzner', 'elzner', 'sargazo');
+                     $db->query("SET NAMES 'utf8'");
+                     $consulta=$db->query("SELECT * FROM sitio;");
+                     while($sitio=$consulta->fetch_assoc()){
+                  ?>
+                  <!--Blog Small Post Start-->
+                  <div class="col-md-6 col-sm-6">
+                     <div class="blog-post">
+                        <div class="blog-thumb"> <a href="#"><i class="fas fa-link"></i></a> <img src="images2/sargazo1.jpg" alt=""> </div>
+                        <div class="post-txt">
+                           <ul class="post-meta">
+                              <li> <a href="#"><i class="fas fa-calendar-alt"></i><?php echo $sitio['FechaCreacion']; ?></a> </li>
+                              <li> <a href="#"><i class="fas "></i><?php echo $sitio['EstadoSitio']; ?></a> </li>
+                           </ul>
+                           <h5><a href="#"><?php echo $sitio['NombreDescriptivo']; ?></a></h5>
+                        </div>
+                     </div>
+                  </div>
+                  <?php
+                     }
+                  ?>
+                  
+               <div class="gt-pagination">
+                 <nav>
+                   <ul class="pagination">
+                     <li class="page-item"> <a class="page-link" href="#" aria-label="Previous"> <i class="fas fa-angle-left"></i> </a> </li>
+                     <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                     <li class="page-item"><a class="page-link" href="#">2</a></li>
+                     <li class="page-item"><a class="page-link" href="#">3</a></li>
+                     <li class="page-item"> <a class="page-link" href="#" aria-label="Next"> <i class="fas fa-angle-right"></i> </a> </li>
+                   </ul>
+                 </nav>
+               </div>
+            </div>
+         </section>
+      </div>
+      <!--   JS Files Start  --> 
+      <script src="js/jquery-3.3.1.min.js"></script> 
+      <script src="js/jquery-migrate-1.4.1.min.js"></script> 
+      <script src="js/popper.min.js"></script> 
+      <script src="js/bootstrap.min.js"></script> 
+      <script src="js/owl.carousel.min.js"></script> 
+      <script src="js/jquery.prettyPhoto.js"></script> 
+      <script src="js/isotope.min.js"></script> 
+      <script src="js/custom.js"></script>
+      <script type="text/javascript">
+         function validarForm(formulario) 
+         {
+             if(formulario.palabra.value.length==0) 
+             { //¿Tiene 0 caracteres?
+                 formulario.palabra.focus();  // Damos el foco al control
+                 alert('Debes rellenar este campo'); //Mostramos el mensaje
+                 return false; 
+              } //devolvemos el foco  
+              return true; //Si ha llegado hasta aquí, es que todo es correcto 
+          }   
+     </script>
+   </body>
+
+
+</html>
